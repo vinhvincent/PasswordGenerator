ebae20594ae0ea80e2e1d2146254e31ce75675ad
vinhvincent <vincentvinh5@gmail.com>
2022-10-08 12:22:37 -0400

    implementation of methods
    Methods for lower and upper case + numeric and special char are availables

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.Console;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println(Methods.Lowercase() + " | " + 
+					   Methods.Uppercase() + " | " + 
+			           Methods.Numeric() + " | " + 
+					   Methods.Special() + " | " + 
+			           Methods.PassGenerator(true, false, true, false, 20));
+	}
+
+}
---
+++ src/Methods.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+
+public class Methods {
+	static char Lowercase() {
+		Random random = new Random();
+		char picked = (char) (random.nextInt(26) + 'a');
+		/*
+		 * the character 'a' corresponds to the number 97,
+		 * using which we can generate 26 random integers that correspond to the lower-case alphabet
+		 */
+		return picked;		
+	}
+	static char Uppercase() {
+		return Character.toUpperCase(Lowercase());		
+	}
+	static int Numeric() {
+		Random random = new Random();
+		int picked = random.nextInt(10); 
+		return picked;		
+	}
+	static char Special() {
+		Random random = new Random();
+		final String special = "!?#$%&*^@";
+		final int len = special.length();
+		char picked = special.charAt(random.nextInt(len));
+		return picked;		
+	}
+	static String PassGenerator(boolean isLower, boolean isUpper, boolean isNumeric, boolean isSpecial, int lenght) {
+		String psw = "";
+		int rnum = 0;
+		Random random = new Random();
+
+		while(psw.length() < lenght) {
+			rnum = random.nextInt(4 - 1 + 1) + 1;
+			if (isLower && rnum == 1) {
+				psw = psw + Lowercase();
+			}
+			else if (isUpper && rnum == 2) {
+				psw = psw + Uppercase();
+			}
+			else if (isNumeric && rnum == 3) {
+				psw = psw + Numeric();
+			}
+			else if (isSpecial && rnum == 4) {
+				psw = psw + Special();
+			}
+			System.out.println("pass : " + psw);
+		}
+		return psw;
+		
+	}
+}
